4cbafc1e99375c149d47a96a7d419c43797e213d
cale312 <zikisacalesogaxa@gmail.com>
2017-05-05 15:09:09 +0200

    move use strict globally...

--- controllers/plateListController.js
+++ controllers/plateListController.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = function(app) {
-
   var mongoose = require('mongoose');
   var db = mongoose.connection;
+  
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', function() {
     console.log('We are connected');
