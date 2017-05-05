10ac61e94274b3f4a5b6e141ef2e058a703efaae
cale312 <zikisacalesogaxa@gmail.com>
2017-05-05 16:37:07 +0200

    moved connection logging

--- controllers/plateListController.js
+++ controllers/plateListController.js
@@ -1,15 +1,17 @@
 'use strict';
 module.exports = function(app) {
   var mongoose = require('mongoose');
+
+
+  const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/regnumbers";
+  mongoose.connect(mongoURL);
+
   var db = mongoose.connection;
 
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', function() {
     console.log('We are connected');
   });
-
-  const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/regnumbers";
-  mongoose.connect(mongoURL);
 
   var plateSchema = mongoose.Schema({
     plate: String,
