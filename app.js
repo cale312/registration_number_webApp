09511ab2fc58ce17591094352f95bfc51f643047
cale312 <zikisacalesogaxa@gmail.com>
2017-08-19 18:33:05 +0200

    something weird

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
-var plateListController = require('./controllers/plateListController');
+var plateListController = require('./controllers/main');
 var app = express();
 
 app.use('/public', express.static('public'));
